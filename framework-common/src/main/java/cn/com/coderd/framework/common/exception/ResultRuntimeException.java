7852bc2a2a3cf416cf700d00db977df82a539cd3
LiShiwu <Lswu2011@foxmail.com>
2023-04-17 16:31:34 +0800

    异常添加调试信息字段

--- framework-common/src/main/java/cn/com/coderd/framework/common/exception/ResultRuntimeException.java
+++ framework-common/src/main/java/cn/com/coderd/framework/common/exception/ResultRuntimeException.java
@@ -2,14 +2,23 @@ package cn.com.coderd.framework.common.exception;
 
 
 import cn.com.coderd.framework.common.basic.Result;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
 /**
  * 业务异常 使用该类如果包含cause并且直接返回消费端,请确保该异常类型存在于消费端代码中
  */
+@Data
+@Accessors(chain = true)
 public class ResultRuntimeException extends RuntimeException {
     private static final long serialVersionUID = -8402422994445822042L;
-    private final String statusCode;
-    private final String statusMessage;
+    private String statusCode;
+    private String statusMessage;
+    private String debugInfo;
+
+    public ResultRuntimeException() {
+
+    }
 
     public ResultRuntimeException(String statusCode, String statusMessage, Object... args) {
         super(String.format("[%s]-[%s]", statusCode, String.format(statusMessage, args)));
@@ -27,14 +36,6 @@ public class ResultRuntimeException extends RuntimeException {
         super(String.format("[%s]-[%s]", statusCode, String.format(statusMessage, args)), cause, false, writableStackTrace);
         this.statusCode = statusCode;
         this.statusMessage = String.format(statusMessage, args);
-    }
-
-    public String getStatusCode() {
-        return statusCode;
-    }
-
-    public String getStatusMessage() {
-        return statusMessage;
     }
 
     public Result<?> toResult() {
